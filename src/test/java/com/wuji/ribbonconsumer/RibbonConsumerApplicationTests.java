ebbb1d7fa239d81fe528efc814a9492b4d36f123
552277 <15090552277@163.com>
2018-06-29 09:00:20 +0800

    服务消费者

---
+++ src/test/java/com/wuji/ribbonconsumer/RibbonConsumerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.wuji.ribbonconsumer;
+
+/*@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RibbonConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}*/
